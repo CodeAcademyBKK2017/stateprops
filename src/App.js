be47bd6a5ea5e2cc90985153a22ecfe94c47e652
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-01 15:49:56 +0700

    update result

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  add = () => {
+    this.setState({count: this.state.count+1}); 
+  }
+  subtract = () => {
+    this.setState({count: this.state.count-1});
+  }
+  state = {
+    count: 0
+  };
   render() {
+
+    console.log('render');
     return (
       <div className="App">
-
+        <input type="text" value={this.state.count} /> 
+        <button onClick={() => this.add()}>ADD</button> <button onClick={() => this.subtract()}>SUBTRACT</button>
       </div>
     );
   }
