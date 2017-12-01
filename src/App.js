877c4383d6275ad497ae93cc2f041e71f9307ce9
Panurut <nubrabra5@gmail.com>
2017-12-01 15:52:37 +0700

    '01122017'

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text : 0,
+    add: 'ADD',
+    subtract: 'SUBTRACT'
+  }
+  addValue = () => {
+    let newValue = this.state.text + 1;
+    this.setState({ text : newValue })
+  }
+  subtractValue = () => {
+    let newValue = this.state.text - 1;
+    this.setState({text : newValue })
+  }
   render() {
     return (
       <div className="App">
-
+        <h1 style={{ fontSize: '40px' }}>{this.state.text}</h1>
+        <div>
+          <button onClick = {this.addValue} >{this.state.add}</button>
+          <button onClick = {this.subtractValue} >{this.state.subtract}</button>
+        </div>
       </div>
     );
   }
---
+++ src/subtractButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Subtract extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Subtract
