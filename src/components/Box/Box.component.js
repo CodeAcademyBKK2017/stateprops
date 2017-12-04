2a16ce8c090415ee7db96f4b11d7a8392fad49cd
deathsky0725 <wbenjapol@gmail.com>
2017-12-04 02:44:23 +0000

    Fix ESLINT

--- src/components/Box/Box.component.js
+++ src/components/Box/Box.component.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Box.style.css';
-class Box extends Component{
-    clickevent= ()=>{
-        return this.props.event;
-       
-    }
-    render(){
-        return(
-            <div className={this.props.type} onClick={this.clickevent()}>{this.props.name}</div>
-        );   
+class Box extends Component {
+    clickevent= () => this.props.event
+    render () {
+    	return (
+    		<div className={this.props.type} onClick={this.clickevent()}>{this.props.name}</div>
+    	);   
     }
 }
 export default Box;
