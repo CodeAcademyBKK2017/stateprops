9cebdf113e0898e751e78e9db4703f7594a76c82
Joe <suphawutk@gmail.com>
2017-12-04 11:10:01 +0700

    'Ok'

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Enzyme, {shallow} from 'enzyme';
-import App from './App';
+import App from '../src/App';
 
 describe('App', () => {
   it('Overlay: renders correctly', () => {
