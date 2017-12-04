2bced8007f2944226cf324b5843aa113046f7a55
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-04 09:37:43 +0700

    update

--- src/components/Counter/Counter.component.js
+++ src/components/Counter/Counter.component.js
@@ -1,49 +1,49 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.css';
 // import {
-//     Collapse,
-//     Navbar,
-//     NavbarToggler,
-//     NavbarBrand,
-//     Nav,
-//     NavItem,
-//     NavLink,
-//     Container,
-//     Row,
-//     Col,
-//     Jumbotron,
-//     Button
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   Container,
+//   Row,
+//   Col,
+//   Jumbotron,
+//   Button
 // } from 'reactstrap';
 // import {
-//     Container,
-//     Row,
-//     Col
+//   Container,
+//   Row,
+//   Col
 // } from 'reactstrap';
 import './Counter.style.css';
 
 class Counter extends React.Component {
-    state = {
-      data: 0
-    }
-    addFn = () => {
-      this.setState({data: this.state.data + 1});
-    }
-    subtractFn = () => {
-      this.setState({data: this.state.data - 1});
-    }
-    render () {
-      return (
-        <div>
-          <div style={{textAlign: 'center'}}>
-            <input className="Counter-box" type="text" value={this.state.data} />
-          </div>
-          <div style={{textAlign: 'center', marginTop: '90px'}}>
-            <button className="Counter-button Counter-button--yellow" onClick={this.addFn}>ADD</button>
-            <button className="Counter-button Counter-button--green" onClick={this.subtractFn}>SUBTRACT</button>
-          </div>
+  state = {
+    data: 0
+  }
+  addFn = () => {
+    this.setState({data: this.state.data + 1});
+  }
+  subtractFn = () => {
+    this.setState({data: this.state.data - 1});
+  }
+  render () {
+    return (
+      <div>
+        <div style={{textAlign: 'center'}}>
+          <input className="Counter-box" type="text" value={this.state.data} />
         </div>
-      );
-    }
+        <div style={{textAlign: 'center', marginTop: '90px'}}>
+          <button className="Counter-button Counter-button--yellow" onClick={this.addFn}>ADD</button>
+          <button className="Counter-button Counter-button--green" onClick={this.subtractFn}>SUBTRACT</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Counter;
