fc48c51c174560cbc4f9eba422b6aa43ccd79dc8
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-04 10:58:09 +0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -11,10 +11,25 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Enzyme, {shallow} from 'enzyme';
 import App from './App';
+import Counter from './components/Counter/Counter.component';
 
 describe('App', () => {
   it('Overlay: renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('add: should increment state', () => {
+    const wrapper = shallow(<Counter />);
+    const instance = wrapper.instance();
+    expect(instance.state.data).toBe(0);
+    instance.addFn();
+    expect(instance.state.data).toBe(1);
+  });
+  it('subtract: should increment state', () => {
+    const wrapper = shallow(<Counter />);
+    const instance = wrapper.instance();
+    expect(instance.state.data).toBe(0);
+    instance.subtractFn();
+    expect(instance.state.data).toBe(-1);
   });
 });
