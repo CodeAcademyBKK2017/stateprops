1c9e1bc493f1dc2c6bb77f543b7fc1c21be3dc65
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-04 10:32:38 +0700

    setup jest with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import Enzyme, {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
