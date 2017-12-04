49a6298daaf0a5215f85add89ad86d8c770597db
deathsky0725 <wbenjapol@gmail.com>
2017-12-04 03:32:39 +0000

    Config Jest with the setup some thing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Box from './components/Box/Box.component';
 class App extends Component {
   state = {
-      value: 0
+    value: 0
   }
-  addclickHandler = ()=>{
-      this.setState({value: this.state.value+1},()=>{
-      });
-  }
-  subclickHandler = ()=>{
-      this.setState({value: this.state.value-1});
-  }
-  render() {
-      return (
-          <div className="App" >
-              <Box type={"DisplayBox"} name={this.state.value}/>
+  addclickHandler = () => this.setState({value: this.state.value + 1});
+  subclickHandler = () => this.setState({value: this.state.value - 1});
+  render () {
+    return (
+      <div className="App" >
+        <Box type={'DisplayBox'} name={this.state.value}/>
 
-              <Box type={"ADD"} name={"ADD"} event={this.addclickHandler}/>
+        <Box type={'ADD'} name={'ADD'} event={this.addclickHandler}/>
 
-              <Box type={"SUBTRACT"} name={"SUBTRACT"} event={this.subclickHandler}/>
-          </div>
-      );
+        <Box type={'SUBTRACT'} name={'SUBTRACT'} event={this.subclickHandler}/>
+      </div>
+    );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('Overlay: renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
